438c48fb9d973afc94c7cb7be3fa315f26767302
Andrei Sidletchi <ASidletchi@ellipsesolutions.com>
2022-04-20 15:14:15 +0300

    test

--- RFPBuilder/Program.cs
+++ RFPBuilder/Program.cs
@@ -17,7 +17,6 @@ namespace RFPBuilder
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            int i;
         }
     }
 }
